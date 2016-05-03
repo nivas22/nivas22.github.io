3adb06eb081a3ddaf2517983d3f516ed19005e04
seenivasan <seenivasan.s@heymath.com>
2016-05-03 16:59:24 +0530

    Update languaeg.js

--- assets/js/language.js
+++ assets/js/language.js
@@ -5,9 +5,9 @@ z.constant('languages',{
     var english = {
 
       'Hero_Name': 'Manthiram',
-      'Heroine_Name':'Gomathi',
+      'Heroine_Name':'Gomathy',
       'Label_Fun': 'Marriage',
-      'Top_Quote': 'Come and Wish US',
+      'Top_Quote': 'Come and Wish US...!',
 
       'Second_Heading': 'Groom & Bride',
       'Line_One':'WE ARE',
@@ -17,39 +17,69 @@ z.constant('languages',{
       'Label_Day': 'Marriage Date',
       'Mrg_Day': 'May 16',
       'Label_Invitaiton': 'Invitation',
-      'Labe_U': 'For U Only',
+      'Labe_U': 'For U Only...!',
       'Label_Description': 'marriage date and further',
       'Label_Address' : 'Address',
       'Label_venue'  :  'JPR Marriage Hall',
-      'Label_Location' : 'Pallayankottai',
-      'Label_District': 'Nellai',
+      'Label_Location' : 'Palayamkottai',
+      'Label_District': 'Tirunelveli',
       
 };
 
       
     return english;
   },
+
+  {
+  ml: function() {
+    var malayalam = {
+
+      'Hero_Name': 'Manthiram',
+      'Heroine_Name':'Gomathy',
+      'Label_Fun': 'വിവാഹം',
+      'Top_Quote': 'വരുവിൻ നാം നേരുന്നു...!',
+
+      'Second_Heading': 'വരന് & മണവാട്ടി',
+      'Line_One':'ഞങ്ങൾ ആകുന്നു',
+      'Sub_Quote': 'വിവാഹം കഴിക്കാൻ പോകുന്നു',
+      'Label_Hero': 'കഥാനായകന്',
+      'Label_Heroine': 'നായികമാരിൽ',
+      'Label_Day': 'വിവാഹ തീയതി',
+      'Mrg_Day': 'മേയ് 16',
+      'Label_Invitaiton': 'ക്ഷണം',
+      'Labe_U': 'നിനക്കായ് മാത്രം...!',
+      'Label_Description': 'marriage date and further',
+      'Label_Address' : 'വിലാസം',
+      'Label_venue'  :  'JPR കല്യാണമണ്ഡപം',
+      'Label_Location' : 'Palayamkottai',
+      'Label_District': 'Tirunelveli',
+      
+};
+
+      
+    return malayalam;
+  },
   tn: function(){
     var tamil = {
-      'Hero_Name': 'sdf',
-      'Heroine_Name':'Gomathi',
-      'Label_Fun': 'Marriage',
-      'Top_Quote': 'Come and Wish US',
+      'Hero_Name': 'மந்திரம்',
+      'Heroine_Name':'கோமதி',
+      'Label_Fun': 'மணவிழா',
+      'Top_Quote': 'வாழ்த்துவோம் வாங்க...!',
 
-      'Second_Heading': 'Groom & Bride',
-      'Line_One':'WE ARE',
-      'Sub_Quote': 'Getting married',
-      'Label_Hero': 'Hero',
-      'Label_Heroine': 'Heroine',
-      'Label_Day': 'Marriage Date',
-      'Mrg_Day': 'May 16',
-      'Label_Invitaiton': 'Invitation',
-      'Labe_U': 'For U Only',
-      'Label_Description': 'marriage date and further',
-      'Label_Address' : 'Address',
-      'Label_venue'  :  'JPR Marriage Hall',
-      'Label_Location' : 'Pallayankottai',
-      'Label_District': 'Nellai',
+      'Second_Heading': 'மணமக்கள்',
+      'Line_One':'நாங்க தான்',
+      'Sub_Quote': 'கல்யாணம் எங்களுக்கு தான்..!',
+      'Label_Hero': 'நாயகன்',
+      'Label_Heroine': 'நாயகி',
+      'Label_Day': 'மண நாள்',
+      'Mrg_Day': 'மே 16',
+      'Label_Invitaiton': 'அழைப்பிதழ்',
+      'Labe_U': 'உங்களுக்கு தான்...!',
+      'Label_Description': 'இந்த மே மாசம் 16ஆம் தேதி திங்ககிழம காலையிலே 9 மணிக்கு மேல 10 மணிக்குள்ள எங்களுக்கு கல்யாணம். சாயும்காலம் 6 மணில இருந்து 8 மணிக்குள்ள வரவேற்ப்பு நடக்கபோகுது. பாசமா கூப்பிடுறோம். குடும்பமா வாங்க. வாழ்த்துக்களையும் ஆசிகளையும் தாங்க.',
+      'Label_Address' : 'முகவரி',
+      'Label_venue'  :  'JPR திருமண மண்டபம்',
+      'Label_Location' : 'பாளையங்கோட்டை',
+      'Label_District': 'திருநெல்வேலி',
     };
     return tamil;
   }
