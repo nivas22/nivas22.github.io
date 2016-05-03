d1c82b37cc704c5e657bda0b17fa49935bdcf8e6
Seenivasan <mailtonivass@gmail.com>
2016-05-03 23:37:14 +0530

    Updated the code

--- assets/js/language.js
+++ assets/js/language.js
@@ -3,27 +3,34 @@
 z.constant('languages',{
   en: function() {
     var english = {
+	  
+	  'Title': 'Manthiram - Gomathy Wedding',
+	  
+	  'H1': 'Home',
+      'H2':'Groom & Bride',
+      'H3': 'Invitation',
+      'H4': 'Contact',
 
       'Hero_Name': 'Manthiram',
       'Heroine_Name':'Gomathy',
-      'Label_Fun': 'Marriage',
-      'Top_Quote': 'Come and Wish US...!',
+      'Label_Fun': 'A Wedding Shower',
+      'Top_Quote': 'Two lives, two hearts joined together in friendship united forever in love',
 
       'Second_Heading': 'Groom & Bride',
-      'Line_One':'WE ARE',
-      'Sub_Quote': 'Getting married',
+      'Line_One':'We Are Getting Married',
+      'Sub_Quote': 'together with their parents and with joyful hearts invite you to share in the happiness as they join their lives in marriage on Monday, 16th May 2016 between 9.00 a.m and 10.00 a.m',
       'Label_Hero': 'Hero',
       'Label_Heroine': 'Heroine',
       'Label_Day': 'Marriage Date',
       'Mrg_Day': 'May 16',
       'Label_Invitaiton': 'Invitation',
-      'Labe_U': 'For U Only...!',
+      'Labe_U': 'For U Only',
       'Label_Description': 'marriage date and further',
       'Label_Address' : 'Address',
       'Label_venue'  :  'JPR Marriage Hall',
       'Label_Location' : 'Palayamkottai',
-      'Label_District': 'Tirunelveli',
-      
+      'Label_District': 'Tirunelveli - 627002',
+      'Footer':'With regard Friends & Relatives...',
 };
 
       
@@ -33,11 +40,18 @@ z.constant('languages',{
   
   ml: function() {
     var malayalam = {
+	
+	'Title': 'மந்திரம்-கோமதி திருமணம்',
+	
+	  'H1': 'Home',
+      'H2':'Groom & Bride',
+      'H3': 'Invitation',
+      'H4': 'Contact',
 
       'Hero_Name': 'Manthiram',
       'Heroine_Name':'Gomathy',
       'Label_Fun': 'വിവാഹം',
-      'Top_Quote': 'വരുവിൻ നാം നേരുന്നു...!',
+      'Top_Quote': 'വരുവിൻ നാം നേരുന്നു.',
 
       'Second_Heading': 'വരന് & മണവാട്ടി',
       'Line_One':'ഞങ്ങൾ ആകുന്നു',
@@ -52,8 +66,8 @@ z.constant('languages',{
       'Label_Address' : 'വിലാസം',
       'Label_venue'  :  'JPR കല്യാണമണ്ഡപം',
       'Label_Location' : 'Palayamkottai',
-      'Label_District': 'Tirunelveli',
-      
+      'Label_District': 'Tirunelveli - 627002',
+      'Footer':'அவ்வண்மை விரும்பும் சுற்றமும் நட்பும்'
 };
 
       
@@ -61,25 +75,34 @@ z.constant('languages',{
   },
   tn: function(){
     var tamil = {
+	
+	  'Title': 'மந்திரம்-கோமதி திருமணம்',
+	  
+	  'H1': 'முகப்பு',
+      'H2':'மணமக்கள்',
+      'H3': 'அழைப்பிதழ்',
+      'H4': 'முகவரி',
+
       'Hero_Name': 'மந்திரம்',
       'Heroine_Name':'கோமதி',
       'Label_Fun': 'மணவிழா',
-      'Top_Quote': 'வாழ்த்துவோம் வாங்க...!',
+      'Top_Quote': 'அவன் வானவில்லாக,அவள் வண்ண நிழலாக கைகோர்க்கும் நேரம்', //'வாழ்த்துவோம் வாங்க',
 
       'Second_Heading': 'மணமக்கள்',
       'Line_One':'நாங்க தான்',
-      'Sub_Quote': 'கல்யாணம் எங்களுக்கு தான்..!',
+      'Sub_Quote': 'கல்யாணம் எங்களுக்கு தான்.',
       'Label_Hero': 'நாயகன்',
       'Label_Heroine': 'நாயகி',
       'Label_Day': 'மண நாள்',
       'Mrg_Day': 'மே 16',
       'Label_Invitaiton': 'அழைப்பிதழ்',
-      'Labe_U': 'உங்களுக்கு தான்...!',
+      'Labe_U': 'உங்களுக்கு தான்',
       'Label_Description': 'இந்த மே மாசம் 16ஆம் தேதி திங்ககிழம காலையிலே 9 மணிக்கு மேல 10 மணிக்குள்ள எங்களுக்கு கல்யாணம். சாயும்காலம் 6 மணில இருந்து 8 மணிக்குள்ள வரவேற்ப்பு நடக்கபோகுது. பாசமா கூப்பிடுறோம். குடும்பமா வாங்க. வாழ்த்துக்களையும் ஆசிகளையும் தாங்க.',
       'Label_Address' : 'முகவரி',
       'Label_venue'  :  'JPR திருமண மண்டபம்',
       'Label_Location' : 'பாளையங்கோட்டை',
-      'Label_District': 'திருநெல்வேலி',
+      'Label_District': 'திருநெல்வேலி - 627002' ,
+	  'Footer':'அவ்வண்மை விரும்பும் சுற்றமும் நட்பும்...',
     };
     return tamil;
   }
